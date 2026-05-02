1d370fab90198edd7cb96a4906f7ff5c2558d50a
Nelson Cruz Mora <nelsoncruzmora@gmail.com>
2026-05-02 14:31:59 -0500

    Added a fetch data function for services

--- StockList/Core/Networking/APIClient.swift
+++ StockList/Core/Networking/APIClient.swift
@@ -8,4 +8,17 @@
 import Foundation
 
 struct APIClient {
+    func fetchData(from url: URL, headers: [String: String] = [:]) async throws -> Data {
+        var request = URLRequest(url: url)
+        for (key, value) in headers {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.invalidResponse
+        }
+
+        return data
+    }
 }
--- StockList/Core/Networking/NetworkError.swift
+++ StockList/Core/Networking/NetworkError.swift
@@ -9,4 +9,5 @@ import Foundation
 
 enum NetworkError: Error {
     case invalidURL
+    case invalidResponse
 }
