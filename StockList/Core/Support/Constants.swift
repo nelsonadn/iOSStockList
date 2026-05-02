e2293b0d45e42295b2cba4adb363e7e5937b8872
Nelson Cruz Mora <nelsoncruzmora@gmail.com>
2026-05-02 14:10:55 -0500

    Added constant files

---
+++ StockList/Core/Support/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StockList
+//
+//  Created by Nelson Cruz Mora on 2/5/26.
+//
+
+import Foundation
+
+enum Constants {
+    static let refreshInterval: TimeInterval = 8
+}
---
+++ StockList/Core/Support/Formatters.swift
@@ -0,0 +1,11 @@
+//
+//  Formatters.swift
+//  StockList
+//
+//  Created by Nelson Cruz Mora on 2/5/26.
+//
+
+import Foundation
+
+enum Formatters {
+}
