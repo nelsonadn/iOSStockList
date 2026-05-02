eff01c4e2b3ba1d53dcd29d2fda0ee1705de36f4
Nelson Cruz Mora <nelsoncruzmora@gmail.com>
2026-05-02 14:32:49 -0500

    Update APIEndpoint.swift

--- StockList/Core/Networking/APIEndpoint.swift
+++ StockList/Core/Networking/APIEndpoint.swift
@@ -8,4 +8,28 @@
 import Foundation
 
 enum APIEndpoint {
+    case marketSummary
+    case stockSummary(symbol: String)
+
+    var path: String {
+        switch self {
+        case .marketSummary:
+            return "/market/get-summary"
+        case .stockSummary:
+            return "/stock/get-summary"
+        }
+    }
+
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .marketSummary:
+            return [URLQueryItem(name: "region", value: "US")]
+        case .stockSummary(let symbol):
+            return [
+                URLQueryItem(name: "lang", value: "en-US"),
+                URLQueryItem(name: "symbol", value: symbol),
+                URLQueryItem(name: "region", value: "US")
+            ]
+        }
+    }
 }
