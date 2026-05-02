e0498de341bac28eea64db5c4353c262b4574dff
Nelson Cruz Mora <nelsoncruzmora@gmail.com>
2026-05-02 13:55:09 -0500

    First commit, started Xcode project

---
+++ StockList/StockListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockListApp.swift
+//  StockList
+//
+//  Created by Nelson Cruz Mora on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct StockListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
